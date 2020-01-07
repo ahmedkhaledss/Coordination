4c25f1f6c123c9de5eb8881fabf16598b59f3ecd
Ahmed Khaled <ahmedkhaled-selim@vodafone.com>
2020-01-07 22:54:58 +0200

    Initial Commit

---
+++ Coordination/Coordination.h
@@ -0,0 +1,19 @@
+//
+//  Coordination.h
+//  Coordination
+//
+//  Created by Ahmed Khaled on 1/7/20.
+//  Copyright © 2020 Ahmed Khaled. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Coordination.
+FOUNDATION_EXPORT double CoordinationVersionNumber;
+
+//! Project version string for Coordination.
+FOUNDATION_EXPORT const unsigned char CoordinationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Coordination/PublicHeader.h>
+
+
